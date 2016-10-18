689d33ea242a540b062a7965946f7e6ac7659219
Oto Brglez <otobrglez@gmail.com>
2016-10-19 01:37:02 +0200

    Adding script for cog

--- tools/hadoop_sync_documents.sh
+++ tools/hadoop_sync_documents.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-find ./data/files | parallel --progress -j 5 "./tools/hadoop_add_document.sh {}"
+find ./data/files | parallel --progress -j 2 "./tools/hadoop_add_document.sh {}"
